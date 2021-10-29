9bb4053954e0327eba55f6998be83e8071171f59
Marshall Heffernan <wetmatches22@gmail.com>
2021-10-29 06:55:34 -0700

    Added test files

---
+++ New Unity Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+  int TestInt = 5;
+
+  void Start()
+  {
+    Debug.Log($"The number is {TestInt}.");
+  }
+
+  void Update()
+  {
+
+  }
+}
